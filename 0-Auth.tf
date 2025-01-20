5a7c22e0c5e1c63e21028454a542f8a551df92db
statuc30721 <bubalator@yahoo.com>
2025-01-20 17:51:21 +0100

    updated aws provider.

--- 0-Auth.tf
+++ 0-Auth.tf
@@ -1,21 +1,19 @@
 # Terraform Setup
 terraform {
   required_providers {
-    /*
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.52.0"
-      configuration_aliases = [ aws.alternate ]
-    }
-    */
 
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.0"
+      #version = "4.52.0"
+      configuration_aliases = [aws.alternate]
     }
+
+
+
     random = {
       source  = "hashicorp/random"
-      version = "3.6.3"
+      version = "3.4.5"
     }
   }
 }
@@ -34,7 +32,61 @@ terraform {
   }
 }
 
+# Default Region for AWS.
 provider "aws" {
   region = "us-east-1"
+
 }
+
+
+# Set default AWS provider in Tokyo.
+provider "aws" {
+  alias  = "tokyo"
+  region = "ap-northeast-1"
+
+}
+
+
+
+# Add additional provier confifuration for each region.
+provider "aws" {
+  alias = "newyork"
+
+  region = "us-east-1"
+}
+
+
+provider "aws" {
+  alias = "london"
+
+  region = "eu-west-2"
+}
+
+provider "aws" {
+  alias = "saopaulo"
+
+  region = "sa-east-1"
+}
+
+
+provider "aws" {
+  alias = "australia"
+
+  region = "ap-southeast-2"
+
+}
+
+provider "aws" {
+  alias = "hongkong"
+
+  region = "ap-east-1"
+
+}
+
+provider "aws" {
+  alias = "california"
+
+  region = "us-west-1"
+}
+
 
